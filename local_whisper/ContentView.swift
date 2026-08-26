1327db39988b5b3c119979b1291314a887530b09
Braulio Fernandes <brauliopf@gmail.com>
2026-08-26 15:16:49 -0700

    Initial Commit

---
+++ local_whisper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  local_whisper
+//
+//  Created by Braulio Pessoa Fernandes on 8/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ local_whisper/local_whisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  local_whisperApp.swift
+//  local_whisper
+//
+//  Created by Braulio Pessoa Fernandes on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct local_whisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
